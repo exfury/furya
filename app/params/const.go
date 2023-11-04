023d2bdcaf30820424b20f8972a756d7ab6613c8
adrian <adrian@fury.black>
2023-11-04 21:45:28 +0530

    CIMMIT

--- app/params/const.go
+++ app/params/const.go
@@ -2,13 +2,13 @@ package params
 
 const (
 	// Name defines the application name of the Furya network.
-	Name = "ufury"
+	Name = "furya"
 
 	// BondDenom defines the native staking token denomination.
 	BondDenom = "ufury"
 
 	// DisplayDenom defines the name, symbol, and display value of the Furya token.
-	DisplayDenom = "FURYA"
+	DisplayDenom = "FURY"
 
 	// DefaultGasLimit - set to the same value as cosmos-sdk flags.DefaultGasLimit
 	// this value is currently only used in tests.
